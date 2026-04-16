5d6046f274d0f7f726dd580da01da90d6b61fed4
Ekaluk52003 <ekaluk.p@gmail.com>
2026-04-16 09:08:53 +0700

    en

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Pre-migration: ensuring assigned_to columns exist..."
-python manage.py shell -c "
-from django.db import connection
-with connection.cursor() as cur:
-    cur.execute(\"\"\"
-        DO \$\$
-        BEGIN
-            IF NOT EXISTS (
-                SELECT 1 FROM information_schema.columns
-                WHERE table_name='core_task' AND column_name='assigned_to_id'
-            ) THEN
-                ALTER TABLE core_task ADD COLUMN assigned_to_id integer NULL;
-            END IF;
-            IF NOT EXISTS (
-                SELECT 1 FROM information_schema.columns
-                WHERE table_name='core_issue' AND column_name='assigned_to_id'
-            ) THEN
-                ALTER TABLE core_issue ADD COLUMN assigned_to_id integer NULL;
-            END IF;
-        END \$\$;
-    \"\"\")
-print('Done.')
-"
-
 echo "Running database migrations..."
 python manage.py migrate --noinput
 
